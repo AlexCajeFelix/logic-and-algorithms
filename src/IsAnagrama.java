cb02e3538541ddae8ab60aa4701e7224da127272
AlexCajeFelix <alexcaje2005@gmail.com>
2025-03-10 11:04:03 -0300

    outra forma de ver se é um anagrama

--- src/IsAnagrama.java
+++ src/IsAnagrama.java
@@ -1,6 +1,8 @@
 
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class IsAnagrama {
     public static void main(String[] args) {
@@ -16,13 +18,35 @@ public class IsAnagrama {
                 s1 = s1.toLowerCase();
                 s2 = s2.toLowerCase();
 
+                if(s1.length() != s1.length()){
+                    return false;
+                }
+
               String vetorS1[] = s1.split("");
               String vetorS2 [] = s2.split("");
 
              Arrays.sort(vetorS1);
              Arrays.sort(vetorS2);
 
-             return Arrays.equals(vetorS1, vetorS2)? true : false;
-               
+             return Arrays.equals(vetorS1, vetorS2);
     }
+
+    public static boolean isAnagramHashTable(String s1, String s2){
+        Map<Character, Integer> strings = new HashMap<>();
+
+        for (char c : s1.toCharArray()) {
+            strings.put(c, strings.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : s2.toCharArray()) {  
+            if (!strings.containsKey(c) || strings.get(c) == 0) {  
+                return false;  
+            }  
+            strings.put(c, strings.get(c) - 1);  
+        }
+                return true;
+            }
+        
+           
+
 }
